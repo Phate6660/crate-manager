6494caa4f114659a14a28d2f8c7cb45f2fb736e5
ValleyKnight <ValleyKnight@protonmail.com>
2021-12-11 19:33:38 -0500

    create list/export commands, show help message if nothing specified

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use std::fs::OpenOptions;
 use std::io::{Read, Write};
 
-fn main() {
-    let user = std::env::var("USER").unwrap();
-    let home = std::env::var("HOME").unwrap_or_else(|_| "/home/".to_string() + &user);
-    let stored_crates = format!("{}/{}", home, "exported_crates.txt");
-    let crates_file = format!("{}/{}", home, "/.cargo/.crates.toml");
+fn list_crates(crates_file: &str) -> Vec<String> {
     let crates = std::fs::File::open(crates_file);
     let crates_list = match crates {
         Ok(mut f) => {
@@ -19,33 +15,54 @@ fn main() {
             crates
         }
     };
-    let mut crates_vec: Vec<(&str, &str)> = Vec::new();
+    let mut crates_vec: Vec<&str> = Vec::new();
     for (idx, line) in crates_list.lines().enumerate() {
         if idx == 0 {
             continue;
         }
         let parts = line.split("=").collect::<Vec<&str>>();
         let name_and_version_string = parts[0].trim();
-        let name = parts[1].trim();
-        crates_vec.push((name_and_version_string, name));
+        crates_vec.push(name_and_version_string);
     }
-    let mut final_crates_vec: Vec<(&str, &str)> = Vec::new();
+    let mut final_crates_vec: Vec<String> = Vec::new();
     for single_crate in crates_vec {
-        let name_and_version_string = single_crate.0;
-        if name_and_version_string.contains("registry") {
-            final_crates_vec.push(single_crate);
+        if single_crate.contains("registry") {
+            final_crates_vec.push(single_crate.to_string());
         }
     }
-    let mut options = OpenOptions::new();
-    let mut stored_crates_file = options
-        .create(true)
-        .append(true)
-        .open(stored_crates)
-        .unwrap();
-    for bin_crate in final_crates_vec {
-        let mut name = bin_crate.0;
-        let name_without_quotes = &name.replace("\"", "");
-        name = name_without_quotes.split(' ').collect::<Vec<&str>>()[0];
-        write!(stored_crates_file, "{}\n", name).unwrap();
+    final_crates_vec
+}
+
+fn main() {
+    let args = std::env::args().collect::<Vec<String>>();
+    let user = std::env::var("USER").unwrap();
+    let home = std::env::var("HOME").unwrap_or_else(|_| "/home/".to_string() + &user);
+    let crates_file = format!("{}/{}", home, "/.cargo/.crates.toml");
+    let crates_vec = list_crates(&crates_file);
+    let na = String::from("N/A");
+    let op = args.get(1).or(Some(&na)).unwrap();
+    if op == "export" {
+        let stored_crates = format!("{}/{}", home, "exported_crates.txt");
+        let mut options = OpenOptions::new();
+        let mut stored_crates_file = options
+            .create(true)
+            .append(true)
+            .open(stored_crates)
+            .unwrap();
+        for bin_crate in crates_vec {
+            let mut name = bin_crate;
+            let name_without_quotes = &name.replace("\"", "");
+            name = name_without_quotes.split(' ').collect::<Vec<&str>>()[0].to_string();
+            writeln!(stored_crates_file, "{}", name).unwrap();
+        }
+    } else if op == "list" {
+        for bin_crate in crates_vec {
+            let mut name = bin_crate;
+            let name_without_quotes = &name.replace("\"", "");
+            name = name_without_quotes.split(' ').collect::<Vec<&str>>()[0].to_string();
+            println!("{}", name);
+        }
+    } else {
+        println!("list (to list installed crates) or export (to export said crates to ~/exported_crates.txt)");
     }
 }
